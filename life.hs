32de2b3be54f2e3aa453d51f9a6a6a36b81e80b4
TJ Takei <tj.takei@gmail.com>
2012-01-06 13:43:11 -0800

    Change a haskell file to replace <$> <*> with liftA2

--- life.hs
+++ life.hs
@@ -1,20 +1,24 @@
 -- Conway's Game of Life
+import Control.Applicative
 import qualified Data.Set as Set
 
 mask :: (Ord t, Num t, Enum t) => (t, t) -> Set.Set (t, t)
-mask (x, y) = Set.fromList [(x', y') | x' <- [x-1..x+1], y' <- [y-1..y+1]]
+mask (x,y) = Set.fromList $ liftA2 (,) (nbr x) (nbr y)
+  where nbr = flip map [-1..1] . (+)
 
 conway :: (Ord t, Num t, Enum t) => Set.Set (t, t) -> (t, t) -> Bool
 conway u c = (Set.size $ Set.intersection u $ mask c) `elem` nlives
   where nlives = if c `Set.member` u then [3, 4] else [3]
 
+dilate :: (Ord t, Num t, Enum t) => Set.Set (t, t) -> Set.Set (t, t)
+dilate = Set.fold (Set.union . mask) Set.empty
+
 lut :: (Ord t, Num t, Enum t) => Set.Set (t, t) -> Set.Set (t, t)
-lut u = Set.filter (conway u) $ dilate u
-  where dilate = Set.fold (Set.union . mask) Set.empty
+lut = liftA2 Set.filter conway dilate
 
 -- a sample test data
 begin :: Set.Set (Int, Int)
-begin = Set.fromList [(0, 1), (-1, 0), (0, 0), (1, 0), (1, -1)]
+begin = Set.fromList [(-1, 0), (0, 0), (0, 1), (1, 0), (1, -1)]
 
 -- see nine lives
 main = mapM_ (putStrLn . show . Set.toList) $ take 9 $ iterate lut begin
